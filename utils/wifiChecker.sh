9b0e18e7bf1595bb4694cc3432c5faf17bcd97b6
LBussy <lee@bussy.org>
2019-01-12 15:03:26 -0600

    Using new includes

--- utils/wifiChecker.sh
+++ utils/wifiChecker.sh
@@ -2,7 +2,7 @@
 
 # Copyright (C) 2018  Lee C. Bussy (@LBussy)
 
-# This file is part of LBussy's BrewPi Script Remix (BrewPi-Script-RMX).
+# This file is part of LBussy's BrewPi Tools Remix (BrewPi-Tools-RMX).
 #
 # BrewPi Script RMX is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with BrewPi Script RMX. If not, see <https://www.gnu.org/licenses/>.
 
-# These scripts were originally a part of brewpi-script, a part of
+# These scripts were originally a part of brewpi-script, an installer for
 # the BrewPi project. Legacy support (for the very popular Arduino
 # controller) seems to have been discontinued in favor of new hardware.
 
@@ -34,44 +34,30 @@
 ### Init
 ############
 
-# Set up some project variables
-THISSCRIPT="wifiChecker.sh"
-VERSION="0.4.5.0"
-# These should stay the same
-PACKAGE="BrewPi-Script-RMX"
+cd "$scriptpath"
+GITROOT="$(git rev-parse --show-toplevel)"
 
-# Support the standard --help and --version.
-#
-# func_usage outputs to stdout the --help usage message.
-func_usage () {
-  echo -e "$PACKAGE $THISSCRIPT version $VERSION
-Usage: sudo . $THISSCRIPT"
-}
-# func_version outputs to stdout the --version message.
-func_version () {
-  echo -e "$THISSCRIPT ($PACKAGE) $VERSION
-Copyright (C) 2018 Lee C. Bussy (@LBussy)
-This is free software: you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published
-by the Free Software Foundation, either version 3 of the License,
-or (at your option) any later version.
-<https://www.gnu.org/licenses/>
-There is NO WARRANTY, to the extent permitted by law."
-}
-if test $# = 1; then
-  case "$1" in
-    --help | --hel | --he | --h )
-      func_usage; exit 0 ;;
-    --version | --versio | --versi | --vers | --ver | --ve | --v )
-      func_version; exit 0 ;;
-  esac
-fi
+# Get project constants
+. "$GITROOT/inc/const.inc"
+
+# Get help and version functionality
+. "$GITROOT/inc/help.inc"
+
+# Get help and version functionality
+. "$GITROOT/inc/asroot.inc"
+
+# Get error handling functionality
+. "$GITROOT/inc/error.inc"
 
 # Print banner if we are not running in cron
 pstree -s $$ | grep -q bash && CRON=false || CRON=true
 if [ "$CRON" = false ]; then
-  echo -e "\n***Script $THISSCRIPT starting.***\n"
+  echo -e "\n***Script $THISSCRIPT starting.***"
 fi
+
+############
+### Function: log{} to add timestamps and log level
+############
 
 function log {
   case $1 in
@@ -86,40 +72,15 @@ function log {
   esac
   msg=$2
   now=$(date '+%Y-%m-%d %H:%M:%S')
-  name=$(echo ${THISSCRIPT^} | awk -F'.' '{print $1}')
+
+  # name = SCRIPTNAME stripped of extension and UPPERCASE
+  name="${THISSCRIPT%.*}" &&   name="${name^^}"
+  # Send "INFO to stdout else (WARN and ERROR) send to stderr
   if [ "$level" = "INFO" ]; then
     echo -e "$now $name $level: $msg"
   else
     echo -e "$now $name $level: $msg" >&2
   fi
-}
-
-### Check if we have root privs to run
-if [[ $EUID -ne 0 ]]; then
-   log 3 "This script must be run as root: sudo ./$THISSCRIPT"
-   exit 1
-fi
-
-############
-### Functions to catch/display errors during runtime
-############
-warn() {
-  local fmt="$1"
-  command shift 2>/dev/null
-  log 3 "$fmt"
-  log 3 "${@}"
-  log 3
-  log 3 "*** ERROR ERROR ERROR ERROR ERROR ***"
-  log 3 "-------------------------------------"
-  log 3 "See above lines for error message."
-  log 3 "Script did not complete."
-  log 3
-}
-
-die () {
-  local st="$?"
-  warn "$@"
-  exit "$st"
 }
 
 ### User-editable settings ###
@@ -132,7 +93,6 @@ INTERVAL=15
 fails=0
 # Get wireless lan device name
 wlan=$(cat /proc/net/wireless | perl -ne '/(\w+):/ && print $1')
-
 if [ "$1" = "--checkinterfaces" ]; then
   # Make sure auto {$wlan} is added to /etc/network/interfaces,
   # otherwise it causes trouble bringing the interface back up
@@ -155,7 +115,7 @@ while [ $fails -lt $MAX_FAILURES ]; do
   ### Try pinging
   ping -c 1 -I $wlan "$gateway" > /dev/null
   if [ $? -eq 0 ]; then
-    log 1 "Successfully pinged $gateway."
+    #log 1 "Successfully pinged $gateway."
     break
   fi
   ### If that didn't work...

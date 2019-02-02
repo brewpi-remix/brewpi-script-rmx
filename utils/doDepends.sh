419be48da261ebe686ad78bdb2db8a8ca0a64a15
lbussy <lee@bussy.org>
2019-02-02 08:59:28 -0600

    Add php5, nginx and Legacy to uninstall process

--- utils/doDepends.sh
+++ utils/doDepends.sh
@@ -4,28 +4,27 @@
 
 # This file is part of LBussy's BrewPi Tools Remix (BrewPi-Tools-RMX).
 #
-# BrewPi Script RMX is free software: you can redistribute it and/or
+# BrewPi Tools RMX is free software: you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation, either version 3 of the
 # License, or (at your option) any later version.
 #
-# BrewPi Script RMX is distributed in the hope that it will be useful,
+# BrewPi Tools RMX is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with BrewPi Script RMX. If not, see <https://www.gnu.org/licenses/>.
+# along with BrewPi Tools RMX. If not, see <https://www.gnu.org/licenses/>.
 
-# These scripts were originally a part of brewpi-script, an installer for
+# These scripts were originally a part of brewpi-tools, an installer for
 # the BrewPi project. Legacy support (for the very popular Arduino
 # controller) seems to have been discontinued in favor of new hardware.
 
-# All credit for the original brewpi-script goes to @elcojacobs,
-# @m-mcgowan, @rbrady, @steersbob, @glibersat, @Niels-R and I'm sure
-# many more contributors around the world. My apologies if I have
-# missed anyone; those were the names listed as contributors on the
-# Legacy branch.
+# All credit for the original brewpi-tools goes to @elcojacobs,
+# @vanosg, @routhcr, @ajt2 and I'm sure many more contributors around
+# the world. My apologies if I have missed anyone; those were the names
+# listed as contributors on the Legacy branch.
 
 # See: 'original-license.md' for notes about the original project's
 # license and credits.
@@ -34,166 +33,327 @@
 ### Init
 ############
 
-# Change to current dir (assumed to be in a repo) so we can get the git info
-pushd . &> /dev/null || exit 1
-cd "$(dirname "$0")" || exit 1 # Move to where the script is
-GITROOT="$(git rev-parse --show-toplevel)" &> /dev/null
-if [ -z "$GITROOT" ]; then
-  echo -e "\nERROR:  Unable to find my repository, did you move this file?"
-  popd &> /dev/null || exit 1
-  exit 1
+# Change to current dir so we can get the git info
+cd "$(dirname "$0")"
+
+# Set up some project constants
+THISSCRIPT="$(basename "$0")"
+SCRIPTNAME="${THISSCRIPT%%.*}"
+VERSION="$(git describe --tags $(git rev-list --tags --max-count=1))"
+GITURL="$(git config --get remote.origin.url)"
+GITPROJ="$(basename $GITURL)" && GITPROJ="${GITPROJ%.*}"
+PACKAGE="${GITPROJ^^}"
+
+# Packages to be uninstalled via apt
+APTPACKAGES="git-core pastebinit build-essential git arduino-core libapache2-mod-php apache2 python-configobj python-dev python-pip php-xml php-mbstring php-cgi php-cli php-common php"
+# nginx packages to be uninstalled via apt
+NGINXPACKAGES="libgd-tools, fcgiwrap, nginx-doc, ssl-cert, fontconfig-config, fonts-dejavu-core, libfontconfig1, libgd3, libjbig0, libnginx-mod-http-auth-pam, libnginx-mod-http-dav-ext, libnginx-mod-http-echo, libnginx-mod-http-geoip, libnginx-mod-http-image-filter, libnginx-mod-http-subs-filter, libnginx-mod-http-upstream-fair, libnginx-mod-http-xslt-filter, libnginx-mod-mail, libnginx-mod-stream, libtiff5, libwebp6, libxpm4, libxslt1.1, nginx, nginx-common, nginx-full"
+# Packages to be uninstalled via pip
+PIPPACKAGES="pyserial psutil simplejson gitpython configobj"
+
+############
+### Functions for --help and --version functionality
+############
+
+# func_usage outputs to stdout the --help usage message.
+func_usage () {
+  echo -e "$PACKAGE $THISSCRIPT version $VERSION
+Usage: sudo ./$THISSCRIPT"
+}
+# func_version outputs to stdout the --version message.
+func_version () {
+  echo -e "$THISSCRIPT ($PACKAGE) $VERSION
+Copyright (C) 2018 Lee C. Bussy (@LBussy)
+This is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+<https://www.gnu.org/licenses/>
+There is NO WARRANTY, to the extent permitted by law."
+}
+if test $# = 1; then
+  case "$1" in
+    --help | --hel | --he | --h )
+      func_usage; exit 0 ;;
+    --version | --versio | --versi | --vers | --ver | --ve | --v )
+      func_version; exit 0 ;;
+  esac
 fi
 
-# Get project constants
-. "$GITROOT/inc/const.inc"
+############
+### Check privilges and permissions
+############
 
-# Get help and version functionality
-. "$GITROOT/inc/help.inc"
+### Check if we have root privs to run
+if [[ $EUID -ne 0 ]]; then
+   echo -e "This script must be run as root: sudo ./$THISSCRIPT" 1>&2
+   exit 1
+fi
 
-# Get help and version functionality
-. "$GITROOT/inc/asroot.inc"
-
-# Get error handling functionality
-. "$GITROOT/inc/error.inc"
-
-# Network test
-. "$GITROOT/inc/nettest.inc"
-
-# Packages to be installed/checked via apt
-APTPACKAGES="git arduino-core git-core pastebinit build-essential apache2 libapache2-mod-php php-cli php-common php-cgi php php-mbstring python-dev python-pip python-configobj php-xml"
-# nginx packages to be uninstalled via apt if present
-NGINXPACKAGES="libgd-tools, fcgiwrap, nginx-doc, ssl-cert, fontconfig-config, fonts-dejavu-core, libfontconfig1, libgd3, libjbig0, libnginx-mod-http-auth-pam, libnginx-mod-http-dav-ext, libnginx-mod-http-echo, libnginx-mod-http-geoip, libnginx-mod-http-image-filter, libnginx-mod-http-subs-filter, libnginx-mod-http-upstream-fair, libnginx-mod-http-xslt-filter, libnginx-mod-mail, libnginx-mod-stream, libtiff5, libwebp6, libxpm4, libxslt1.1, nginx, nginx-common, nginx-full"
-# Packages to be installed/check via pip
-PIPPACKAGES="pyserial psutil simplejson configobj gitpython"
+############
+### Start the script
+############
 
 echo -e "\n***Script $THISSCRIPT starting.***"
+cd ~ # Start from home
 
 ############
-### Check last apt update date
+### Cleanup cron
 ############
 
-# Run 'apt update' if last run was > 1 week ago
-lastUpdate=$(stat -c %Y /var/lib/apt/lists)
-nowTime=$(date +%s)
-if [ $(($nowTime - $lastUpdate)) -gt 604800 ] ; then
-  echo -e "\nLast apt update was over a week ago. Running apt update before updating"
-  echo -e "dependencies."
-  apt update||die
-  echo
+# Clear out the old brewpi cron if it exists
+if [ -f /etc/cron.d/brewpi ]; then
+  echo -e "\nResetting cron."
+  sudo rm -f /etc/cron.d/brewpi
+  sudo /etc/init.d/cron restart
 fi
 
 ############
-### Remove php5 packages if installed
+### Stop running BrewPi instances
 ############
 
-echo -e "\nChecking for previously installed php5 packages."
-# Get list of installed packages
-php5packages="$(dpkg --get-selections | awk '{ print $1 }' | grep 'php5')"
-if [[ -z "$php5packages" ]] ; then
-  echo -e "\nNo php5 packages found."
-else
-  echo -e "\nFound php5 packages installed.  It is recomended to uninstall all php before"
-  echo -e "proceeding as BrewPi requires php7 and will install it during the install"
-  read -p "process.  Would you like to clean this up before proceeding?  [Y/n]: " yn  < /dev/tty
-  case $yn in
-    [Nn]* )
-      echo -e "\nUnable to proceed with php5 installed, exiting.";
-      exit 1;;
-    * )
-      php_packages="$(dpkg --get-selections | awk '{ print $1 }' | grep 'php')"
-      # Loop through the php5 packages that we've found
-      for pkg in ${php_packages,,}; do
-        echo -e "\nRemoving '$pkg'.\n"
-        sudo apt remove --purge $pkg -y
+# Stop (kill) brewpi
+sudo touch /var/www/html/do_not_run_brewpi
+if pgrep -u brewpi >/dev/null 2>&1; then
+  echo -e "\nAttempting gracefull shutdown of process(es) $(pgrep -u brewpi)."
+  cmd="sudo kill -15 $(pgrep -u brewpi)"
+  eval $cmd
+  sleep 2
+  if pgrep -u brewpi >/dev/null 2>&1; then
+    echo -e "Trying a little harder to terminate process(es) $(pgrep -u brewpi)."
+    cmd="sudo kill -2 $(pgrep -u brewpi)"
+    eval $cmd
+    sleep 2
+    if pgrep -u brewpi >/dev/null 2>&1; then
+      echo -e "Being more forcefull with process(es) $(pgrep -u brewpi)."
+      cmd="sudo kill -1 $(pgrep -u brewpi)"
+      eval $cmd
+      sleep 2
+      while pgrep -u brewpi >/dev/null 2>&1;
+      do
+        echo -e "Being really insistent about killing process(es) $(pgrep -u brewpi) now."
+        echo -e "(I'm going to keep doing this till the process(es) are gone.)"
+        cmd="sudo kill -9 $(pgrep -u brewpi)"
+        eval $cmd
+        sleep 2
       done
-	  echo -e "\nCleanup of the php environment complete."
-      ;;
-  esac
+    fi
+  fi
 fi
 
 ############
-### Remove nginx packages if installed
+### Delete repos
 ############
 
-echo -e "\nChecking for previously installed nginx packages."
-# Get list of installed packages
-nginxPackage="$(dpkg --get-selections | awk '{ print $1 }' | grep 'nginx')"
-if [[ -z "$nginxPackage" ]] ; then
-  echo -e "\nNo nginx packages found."
-else
-  echo -e "\nFound nginx packages installed.  It is recomended to uninstall nginx before"
-  echo -e "proceeding as BrewPi requires apache2 and they will conflict with each other."
-  read -p "Would you like to clean this up before proceeding?  [Y/n]: " yn  < /dev/tty
-  case $yn in
-    [Nn]* )
-      echo -e "\nUnable to proceed with nginx installed, exiting.";
-      exit 1;;
-    * )
-      # Loop through the php5 packages that we've found
-      for pkg in ${NGINXPACKAGES,,}; do
-        echo -e "\nRemoving '$pkg'.\n"
-        sudo apt remove --purge $pkg -y
+# Wipe out all the directories
+if [ -d /home/pi/brewpi-tools-rmx ]; then
+  echo -e "\nClearing /home/pi/brewpi-tools-rmx."
+  sudo rm -fr /home/pi/brewpi-tools-rmx
+fi
+if [ -d /home/pi/brewpi-tools ]; then
+  echo -e "\nClearing /home/pi/brewpi-tools."
+  sudo rm -fr /home/pi/brewpi-tools-rmx
+fi
+if [ -d /home/brewpi ]; then
+  echo -e "\nClearing /home/brewpi."
+  sudo rm -fr /home/brewpi
+fi
+# Wipe out www if it exists and is not empty
+if [ -d /var/www/html ]; then
+  if [ ! -z "$(ls -A /var/www/html)" ]; then
+    echo -e "\nClearing /var/www/html."
+    sudo rm -fr /var/www/html
+	# Re-create html directory
+    sudo mkdir /var/www/html
+    sudo chown www-data:www-data /var/www/html
+  fi
+fi
+
+############
+### Remove brewpi user/group config
+############
+
+username="pi"
+if getent group brewpi | grep &>/dev/null "\b${username}\b"; then
+  echo -e "\nRemoving $username from brewpi group."
+  sudo deluser pi brewpi
+fi
+if getent group www-data | grep &>/dev/null "\b${username}\b"; then
+  echo -e "\nRemoving $username from www-data group."
+  sudo deluser pi www-data
+fi
+username="www-data"
+if getent group brewpi | grep &>/dev/null "\b${username}\b"; then
+  echo -e "\nRemoving $username from brewpi group."
+  sudo deluser www-data brewpi
+fi
+username="brewpi"
+if getent group www-data | grep &>/dev/null "\b${username}\b"; then
+  echo -e "\nRemoving $username from www-data group."
+  sudo deluser brewpi www-data
+fi
+if sudo id "$username" > /dev/null 2>&1; then
+  echo -e "\nRemoving user $username."
+  sudo userdel "$username"
+fi
+egrep -i "^$username" /etc/group;
+if [ $? -eq 0 ]; then
+   groupdel "$username"
+fi
+
+############
+### Reset Apache
+############
+
+# Reset Apache config to stock
+if [ -f /etc/apache2/apache2.conf ]; then
+  if grep -qF "KeepAliveTimeout 99" /etc/apache2/apache2.conf; then
+    echo -e "\nResetting /etc/apache2/apache2.conf."
+    sudo sed -i -e 's/KeepAliveTimeout 99/KeepAliveTimeout 5/g' /etc/apache2/apache2.conf
+    sudo /etc/init.d/apache2 restart
+  fi
+fi
+
+############
+### (Optional) Package Removal
+############
+
+echo
+read -p "Would you like to remove apt and pip packages? [y/N] :" yn < /dev/tty
+if [[ "$yn" == [Yy]* ]]; then
+  # Remove apt and pip packages
+  
+  ############
+  ### Remove pip packages
+  ############
+  
+  echo -e "\nChecking for pip packages installed with BrewPi."
+  if pip &>/dev/null; then
+    pipInstalled=$(sudo pip list --format=legacy)
+    if [ $? -eq 0 ]; then
+      pipInstalled=$(echo "$pipInstalled" | awk '{ print $1 }')
+      for pkg in ${PIPPACKAGES,,}; do
+        if [[ ${pipInstalled,,} == *"$pkg"* ]]; then
+          echo -e "\nRemoving '$pkg'.\n"
+          sudo pip uninstall $pkg -y
+        fi
       done
-	  echo -e "\nCleanup of the nginx environment complete."
-      ;;
-  esac
-fi
-
-############
-### Install and update required packages
-############
-
-# Now install any necessary packages if they are not installed
-echo -e "\nChecking and installing required dependencies via apt."
-for pkg in ${APTPACKAGES,,}; do
-  pkgOk=$(dpkg-query -W --showformat='${Status}\n' ${pkg,,} | \
-    grep "install ok installed")
-  if [ -z "$pkgOk" ]; then
-    echo -e "\nInstalling '$pkg'.\n"
-    apt install ${pkg,,} -y||die
-    echo
+    fi
   fi
-done
+  
+  ############
+  ### Remove apt packages
+  ############
+  
+  echo -e "\nChecking for apt packages installed with BrewPi."
+  # Get list of installed packages
+  packagesInstalled=$(sudo dpkg --get-selections | awk '{ print $1 }')
+  # Loop through the required packages and uninstall those in $APTPACKAGES
+  for pkg in ${APTPACKAGES,,}; do
+    if [[ ${packagesInstalled,,} == *"$pkg"* ]]; then
+      echo -e "\nRemoving '$pkg'.\n"
+      sudo apt remove --purge $pkg -y
+    fi
 
-# Get list of installed packages with upgrade available
-upgradesAvail=$(dpkg --get-selections | xargs apt-cache policy {} | \
-  grep -1 Installed | sed -r 's/(:|Installed: |Candidate: )//' | \
-  uniq -u | tac | sed '/--/I,+1 d' | tac | sed '$d' | sed -n 1~2p)
-
-# Loop through only the required packages and see if they need an upgrade
-for pkg in ${APTPACKAGES,,}; do
-  if [[ ${upgradesAvail,,} == *"$pkg"* ]]; then
-    echo -e "\nUpgrading '$pkg'.\n"
-    apt upgrade ${pkg,,} -y||die
-    doCleanup=1
-  fi
-done
-
-# Cleanup if we updated packages
-if [ -n "$doCleanup" ]; then
-  echo -e "\nCleaning up local repositories."
-  apt clean -y||warn
-  apt autoclean -y||warn
-  apt autoremove --purge -y||warn
-else
-  echo -e "\nNo apt updates to apply."
-fi
-
-# Install any Python packages not installed, update those installed
-echo -e "\nChecking and installing required dependencies via pip."
-pipcmd='pipInstalled=$(pip list --format=legacy)'
-eval "$pipcmd"
-pipcmd='pipInstalled=$(echo "$pipInstalled" | cut -f1 -d" ")'
-eval "$pipcmd"
-for pkg in ${PIPPACKAGES,,}; do
-  if [[ ! ${pipInstalled,,} == *"$pkg"* ]]; then
-    echo -e "\nInstalling '$pkg'."
-    pip install $pkg||die
+  ############
+  ### Remove php5 packages if installed
+  ############
+  
+  echo -e "\nChecking for previously installed php5 packages."
+  # Get list of installed packages
+  php5packages="$(dpkg --get-selections | awk '{ print $1 }' | grep 'php5')"
+  if [[ -z "$php5packages" ]] ; then
+    echo -e "\nNo php5 packages found."
   else
-    echo -e "\nChecking for update to '$pkg'.\n"
-    pip install $pkg --upgrade||die
+    echo -e "\nFound php5 packages installed.  It is recomended to uninstall all php before"
+    echo -e "proceeding as BrewPi requires php7 and will install it during the install"
+    read -p "process.  Would you like to clean this  up? before proceeding?  [Y/n]: " yn  < /dev/tty
+    case $yn in
+      [Nn]* )
+        echo -e "\nUnable to proceed with php5 installed, exiting.";
+        exit 1;;
+      * )
+        php_packages="$(dpkg --get-selections | awk '{ print $1 }' | grep 'php')"
+        # Loop through the php5 packages that we've found
+        for pkg in ${php_packages,,}; do
+          echo -e "\nRemoving '$pkg'.\n"
+          #sudo apt remove --purge $pkg -y
+        done
+        echo -e "\nCleanup of the php environment complete."
+        ;;
+    esac
   fi
-done
+
+  ############
+  ### Remove nginx packages if installed
+  ############
+  
+  echo -e "\nChecking for previously installed nginx packages."
+  # Get list of installed packages
+  nginxPackage="$(dpkg --get-selections | awk '{ print $1 }' | grep 'nginx')"
+  if [[ -z "$nginxPackage" ]] ; then
+    echo -e "\nNo nginx packages found."
+  else
+    echo -e "\nFound nginx packages installed.  It is recomended to uninstall nginx before"
+    echo -e "proceeding as BrewPi requires apache2 and they will conflict with each other."
+    read -p "Would you like to clean this up before proceeding?  [Y/n]: " yn  < /dev/tty
+    case $yn in
+      [Nn]* )
+        echo -e "\nUnable to proceed with nginx installed, exiting.";
+        exit 1;;
+      * )
+        # Loop through the php5 packages that we've found
+        for pkg in ${NGINXPACKAGES,,}; do
+          echo -e "\nRemoving '$pkg'.\n"
+          sudo apt remove --purge $pkg -y
+        done
+        echo -e "\nCleanup of the nginx environment complete."
+        ;;
+    esac
+  fi
+fi
+  
+############
+### Cleanup repos
+############
+
+# Cleanup
+echo -e "Cleaning up local packages."
+sudo apt clean -y
+sudo apt autoclean -y
+sudo apt autoremove --purge -y
+
+############
+### Change hostname
+###########
+
+oldHostName=$(hostname)
+newHostName="raspberrypi"
+echo -e "\nResetting hostname."
+if [ "$oldHostName" != "$newHostName" ]; then
+  sed1="sudo sed -i 's/$oldHostName/$newHostName/g' /etc/hosts"
+  sed2="sudo sed -i 's/$oldHostName/$newHostName/g' /etc/hostname"
+  eval $sed1
+  eval $sed2
+  sudo hostnamectl set-hostname $newHostName
+  sudo /etc/init.d/avahi-daemon restart
+  echo -e "\nYour hostname has been changed back to '$newHostName'.\n"
+  echo -e "(If your hostname is part of your prompt, your prompt will"
+  echo -e "not change untill you log out and in again.  This will have"
+  echo -e "no effect on anything but the way the prompt looks.)"
+  sleep 3
+fi
+
+############
+### Reset password
+###########
+
+echo -e "\nResetting password for 'pi' back to 'raspberry'."
+echo "pi:raspberry" | sudo chpasswd
+
+############
+### Work Complete
+###########
 
 echo -e "\n***Script $THISSCRIPT complete.***"
 

ef5aade37192833ec7187e97cec79a6cc5fb9f61
Lee Bussy <lee@bussy.org>
2019-12-21 11:47:22 -0600

    Update pip to pip3
    Checking installed packages was still using pip instead of pip3.

--- utils/doDepends.sh
+++ utils/doDepends.sh
@@ -214,7 +214,7 @@ do_packages() {
     
     # Install any Python packages not installed, update those installed
     echo -e "\nChecking and installing required dependencies via pip3."
-    pipcmd='pipInstalled=$(pip list --format=columns)'
+    pipcmd='pipInstalled=$(pip3 list --format=columns)'
     eval "$pipcmd"
     pipcmd='pipInstalled=$(echo "$pipInstalled" | cut -f1 -d" ")'
     eval "$pipcmd"

07c9c5ca3ff07da2224bdee8030196e9776d8d3e
lbussy <lee@bussy.org>
2020-12-29 17:16:26 -0600

    Fix start directory for venv

--- utils/doBrewPi.sh
+++ utils/doBrewPi.sh
@@ -26,15 +26,28 @@ declare GITROOT
 init() {
     # Change to current dir (assumed to be in a repo) so we can get the git info
     pushd . &> /dev/null || exit 1
-    cd "$(dirname $(readlink -e $0))" || exit 1 # Move to where the script is
+    SCRIPTPATH="$( cd "$(dirname "$0")" || exit 1 ; pwd -P )"
+    cd "$SCRIPTPATH" || exit 1 # Move to where the script is
     GITROOT="$(git rev-parse --show-toplevel)" &> /dev/null
     if [ -z "$GITROOT" ]; then
-        echo -e "\nERROR:  Unable to find my repository, did you move this file?"
+        echo -e "\nERROR: Unable to find my repository, did you move this file or not run as root?"
         popd &> /dev/null || exit 1
         exit 1
     fi
 
+    # Get project constants
+    # shellcheck source=/dev/null
+    . "$GITROOT/inc/const.inc" "$@"
+
+    # Get BrewPi user directory
+    USERROOT=$(echo "$GITROOT" | cut -d "/" -f-3)
+
+    # Get error handling functionality
+    # shellcheck source=/dev/null
+    . "$GITROOT/inc/error.inc" "$@"
+
     # Get help and version functionality
+    # shellcheck source=/dev/null
     . "$GITROOT/inc/help.inc" "$@"
 }
 
@@ -43,10 +56,9 @@ init() {
 ############
 
 loop() {
-    local script stdOut stdErr python
+    local script python
     script="$GITROOT/brewpi.py"
-    python="$GITROOT/venv/bin/python3"
-
+    python="$USERROOT/venv/bin/python3"
 
     while :
     do

74c6b0ce88e6626b318927b14e333b657e15c24b
lbussy <lee@bussy.org>
2021-03-29 12:16:07 -0500

    New file

---
+++ utils/doFlash.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# Copyright (C) 2018, 2019 Lee C. Bussy (@LBussy)
+
+# This file is part of LBussy's BrewPi Script Remix (BrewPi-Script-RMX).
+#
+# BrewPi Script RMX is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# BrewPi Script RMX is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with BrewPi Script RMX. If not, see <https://www.gnu.org/licenses/>.
+
+# Declare this script's constants
+declare SCRIPTPATH GITROOT
+# Declare /inc/const.inc file constants
+declare THISSCRIPT SCRIPTNAME VERSION GITROOT GITURL GITPROJ PACKAGE
+# Declare /inc/asroot.inc file constants
+declare HOMEPATH REALUSER
+
+############
+### Init
+############
+
+init() {
+    # Change to current dir (assumed to be in a repo) so we can get the git info
+    pushd . &> /dev/null || exit 1
+    SCRIPTPATH="$( cd "$(dirname "$0")" || exit 1 ; pwd -P )"
+    cd "$SCRIPTPATH" || exit 1 # Move to where the script is
+    GITROOT="$(git rev-parse --show-toplevel)" &> /dev/null
+    if [ -z "$GITROOT" ]; then
+        echo -e "\nERROR: Unable to find my repository, did you move this file or not run as root?"
+        popd &> /dev/null || exit 1
+        exit 1
+    fi
+
+    # Get project constants
+    # shellcheck source=/dev/null
+    . "$GITROOT/inc/const.inc" "$@"
+
+    # Get error handling functionality
+    # shellcheck source=/dev/null
+    . "$GITROOT/inc/error.inc" "$@"
+
+    # Get help and version functionality
+    # shellcheck source=/dev/null
+    . "$GITROOT/inc/asroot.inc" "$@"
+
+    # Get help and version functionality
+    # shellcheck source=/dev/null
+    . "$GITROOT/inc/help.inc" "$@"
+
+    # Get config file read functionality
+    # shellcheck source=/dev/null
+    . "$GITROOT/inc/config.inc" "$@"
+}
+
+############
+### Create a banner
+############
+
+banner() {
+    local adj
+    adj="$1"
+    echo -e "\n***Script $THISSCRIPT $adj.***"
+}
+
+############
+### Flash controller
+############
+
+flash() {
+    local yn branch pythonpath
+    branch="${GITBRNCH,,}"
+    if [ ! "$branch" == "master" ]; then
+        branch="--beta"
+    else
+        branch=""
+    fi
+
+    # TODO:  Determine if we are in multi-chamber
+
+    if [ -n "$CHAMBER" ]; then
+        pythonpath=$(which python)
+    else
+        pythonpath="/home/brewpi/venv/bin/python"
+    fi
+
+    eval "$pythonpath -u $GITROOT/utils/updateFirmware.py $branch"
+}
+
+############
+### Main function
+############
+
+main() {
+    init "$@"   # Init and call supporting libs
+    const "$@"  # Get script constants
+    asroot      # Make sure we are running with root privs
+    help "$@"   # Process help and version requests
+    banner "starting"
+    flash       # Flash firmware
+    banner "complete"
+}
+
+main "$@" && exit 0

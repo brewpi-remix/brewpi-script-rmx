c4d4fb9f922e14d4a679f374a943bc0c1f02d4f0
lbussy <lee@bussy.org>
2021-03-31 17:44:37 -0500

    Hard code brewpi home path

--- utils/doDepends.sh
+++ utils/doDepends.sh
@@ -353,6 +353,8 @@ do_aliases() {
         echo "$activateAlias'. $USERROOT/venv/bin/activate'" >> "$aliasFile"
     fi
 
+    echo -e "$HOMEPATH"
+    read -rp "DEBUG Pause: " yn  < /dev/tty
     menuAlias="alias brewpi="
     aliasFile="$HOMEPATH/.bash_aliases"
     if ! grep "^$menuAlias" "$aliasFile" &>/dev/null; then
